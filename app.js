9607cb2008fcb64b1914586f94d6a5e28060002e
Ibrahim Mondal <ibrahimmondal750@gmail.com>
2024-10-18 21:54:15 +0530

    all jsvascrip done

--- app.js
+++ app.js
@@ -16,10 +16,9 @@ function showItem (user = [] ) {
             <img src=${user.strDrinkThumb} alt="">
            <div class="p-3"> 
             <h3 class="text-white text-[20px]">${user.strDrink}</h3>
-            <p class="text-[15px] text-white my-3">${user.strInstructions.slice(0, 90)}...</p>
+            <p class="text-[15px] text-white my-3 h-[100px]">${user.strInstructions.slice(0, 90)}...</p>
             <div class="my-5">
-              <a href="#" class="bg-green-600 hover:bg-green-500 text-white px-4 py-2 rounded">Watch</a>
-            <a href="#" class="text-white ml-3">View Cocktail</a>
+            <a href="#" class="text-white ml-3" onclick="showPopup('${user.idDrink}')">View Details</a>
             </div>
            </div>
          </div>
@@ -28,9 +27,45 @@ function showItem (user = [] ) {
    }
 };
 
+function showPopup (id){
+    let apiUrl = `https://thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+    fetch(apiUrl)
+    .then((res) => res.json())
+     .then((item) =>  popupDetails(item.drinks[0])    
+)
+}
 
+function popupDetails (meal) {
+   console.log(meal);
+   const popup = document.getElementById("popup");
+//    popup.style.display = "none";
+//    popup.style.display = "block";
+   popup.classList.add("visible")
+   popup.classList.remove("invisible")
+popup.innerHTML = `
+           <div class="popup bg-white w-170%] min-h-[500px] p-10">
+            <img src=${meal.strDrinkThumb} alt="" class="w-[300px] pt-2">
+            <h2 class="text-2xl font-bold mb-4">${meal.strDrink}</h2>
+            <div class="flex gap-2 items-center">
+            <h1>Category:</h1>
+            <h1 class="text-[15px] mb-4 text-red-900 pt-5">${meal.strCategory}</h1>
+            </div>
+            <p class="mb-6">${meal.strInstructions}.</p>
+            <button
+              class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded" onclick="closePopup()"
+            >
+              Close
+            </button>
+          </div>
+   `
+}
 
-
+function closePopup() {
+    popup.classList.add("invisible");
+    popup.classList.remove("visible");
+    // popup.style.display = "block";
+    // popup.style.display = "flex";
+}
 
 
 // // search start 
@@ -77,27 +112,34 @@ function lookUpDetails (id) {
 function showMoreDetails (cocktail){
 //    console.log(cocktail);
    const details = document.getElementById("details");
-   details.style.display = "none";
-   details.style.display = "block";
-//    details.classList.add("invisible")
+//    details.style.display = "none";
+//    details.style.display = "block";
+   details.classList.add("visible")
+   details.classList.remove("invisible")
    details.innerHTML = `
-    <div class="bg-white rounded-lg p-6 w-[50%] md:flex-col md:flex">
-     <img src=${cocktail.strDrinkThumb} alt="">
-        <h2 class="text-lg font-bold">${cocktail.strDrink}</h2>
-        <p class="mt-2">${cocktail.strInstructions}</p>
-        <div class="mt-4 flex justify-start">
-            <button class="bg-red-500 text-white px-4 py-2 rounded" onclick="close()">Close</button>
-        </div>
-    </div>
+            <div class="popup bg-white w-170%] min-h-[500px] p-10">
+            <img src=${cocktail.strDrinkThumb} alt="" class="w-[200px] pt-2">
+            <h2 class="text-2xl font-bold mb-4">${cocktail.strDrink}</h2>
+             <div class="flex gap-2 items-center">
+            <h1>Category:</h1>
+            <h1 class="text-[15px] mb-4 text-red-900 pt-5">${cocktail.strCategory}</h1>
+            </div>
+            <p class="mb-6">${cocktail.strInstructions}</p>
+            <button
+              class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded" onclick="close()"
+            >
+              Close
+            </button>
+            </div>
    `
 }
 function close() {
-    // details.classList.add("invisible");
-    // details.classList.remove("visible");
-    details.style.display = "block";
-    details.style.display = "flex";
-}
+    details.classList.add("visible");
+    details.classList.remove("visible");
+    // details.style.display = "block";
+    // details.style.display = "flex";
+};
 const search = document.getElementById("search");
 search.addEventListener("click", () => {
     fetchCocktail ()
-})
+});
